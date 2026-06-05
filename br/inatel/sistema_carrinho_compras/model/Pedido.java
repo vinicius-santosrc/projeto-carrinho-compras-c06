ef4be3d564bb42669712c0d4cc3f916455713637
Marcelo <mmarcelohenriquegmh@gmail.com>
2026-06-05 19:54:51 -0300

    Update Pedido.java

--- br/inatel/sistema_carrinho_compras/model/Pedido.java
+++ br/inatel/sistema_carrinho_compras/model/Pedido.java
@@ -9,7 +9,7 @@ public class Pedido {
     private int id;
     private double valorTotal;
 
-    public Pedido(int id, Date data, double valorTotal) {
+    public Pedido(int id, double valorTotal) {
         this.id = id;
         this.valorTotal = valorTotal;
     }
